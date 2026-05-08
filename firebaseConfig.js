62c699ee03eba3ae059c93e173c8ed1a9340e69a
Juli Abdurahman <JULIABDURRAHMAN02@GMAIL.COM>
2026-05-09 00:30:13 +0700

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Ymb-FglhYD2mGUzWNiMh2r08vul4hFI",
+  authDomain: "mutiara-finance.firebaseapp.com",
+  projectId: "mutiara-finance",
+  storageBucket: "mutiara-finance.appspot.com", // 🔥 ini juga aku betulin
+  messagingSenderId: "846315686392",
+  appId: "1:846315686392:web:ff64767c3c5a96df1aae0d",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
